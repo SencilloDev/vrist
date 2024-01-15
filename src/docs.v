9ff4df1bcf94ab8273be980393d8606dfdf3157c
John Hooks <hooksie11@gmail.com>
2024-01-14 22:49:16 -0500

    updates

--- src/docs.v
+++ src/docs.v
@@ -1,8 +1,9 @@
-module vrist 
+module vrist
 
 struct Document {
-	id int 
-	name string 
-	access string 
-	is_pinned bool @[json: "isPinned"]
+	id        int
+	name      string
+	access    string
+	is_pinned bool   @[json: 'isPinned']
+	url_id    string @[json: 'urlId']
 }
--- src/workspaces.v
+++ src/workspaces.v
@@ -1,29 +1,29 @@
-module vrist 
+module vrist
 
-import net.urllib 
+import net.urllib
 import net.http
-import json 
+import json
 
-type WorkspaceRequest = Request 
+type WorkspaceRequest = Request
 
 pub struct Workspace {
 pub:
-	id int 
-	name string 
-	access string 
-	docs []Document 
-	org_domain string @[json: "org_domain"]
+	id         int
+	name       string
+	access     string
+	docs       []Document
+	org_domain string     @[json: 'org_domain']
 }
 
 pub fn (r Request) workspaces() WorkspaceRequest {
-	url := urllib.URL {
-		scheme: r.scheme,
-		host: r.host,
-		path: "/api/workspaces"
+	url := urllib.URL{
+		scheme: r.scheme
+		host: r.host
+		path: '/api/workspaces'
 	}
 	mut req := WorkspaceRequest{}
 	req = r
-	req.method = http.Method.get 
+	req.method = http.Method.get
 	req.url = url
 
 	return req
@@ -31,8 +31,8 @@ pub fn (r Request) workspaces() WorkspaceRequest {
 
 pub fn (w WorkspaceRequest) get(id string) !Workspace {
 	mut req := Request{}
-	req = w 
-	req.url.path += "/${id}"
+	req = w
+	req.url.path += '/${id}'
 
 	resp := req.send()!
 	return json.decode(Workspace, resp)!
@@ -40,23 +40,25 @@ pub fn (w WorkspaceRequest) get(id string) !Workspace {
 
 pub fn (w WorkspaceRequest) update_name(id string, name string) ! {
 	mut req := Request{}
-	req = w 
-	req.url.path += "/${id}"
-	req.method = http.Method.patch 
-	req.data = json.encode({"name": name})
-	
+	req = w
+	req.url.path += '/${id}'
+	req.method = http.Method.patch
+	req.data = json.encode({
+		'name': name
+	})
+
 	req.send()!
 
-	return 
+	return
 }
 
 pub fn (w WorkspaceRequest) delete(id string) ! {
 	mut req := Request{}
-	req = w 
-	req.url.path += "/${id}"
-	req.method = http.Method.delete 
-	
+	req = w
+	req.url.path += '/${id}'
+	req.method = http.Method.delete
+
 	req.send()!
 
-	return 
+	return
 }
